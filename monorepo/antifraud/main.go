a599782e0933f70b106578a769fd9761273721d3
dasilvaoscar <oscarkaka222@gmail.com>
2025-10-05 02:45:11 -0300

    refactor: split goroutine function

--- monorepo/antifraud/main.go
+++ monorepo/antifraud/main.go
@@ -3,8 +3,25 @@ package main
 import (
 	"concurrency-simulator/monorepo/antifraud/handler"
 	"concurrency-simulator/monorepo/antifraud/utils"
+	"log"
 	"sync"
 )
+
+func execution(wg *sync.WaitGroup, logger *log.Logger) {
+	defer wg.Done()
+	consumer, err := handler.NewPaymentConsumer()
+
+	if err != nil {
+		logger.Fatalf("Failed to create Kafka consumer: %v", err)
+	}
+
+	defer consumer.Close()
+
+	logger.Println("Starting Kafka consumer...")
+	if err := consumer.Start(); err != nil {
+		logger.Fatalf("Failed to start Kafka consumer: %v", err)
+	}
+}
 
 func main() {
 	logger := utils.NewLogger()
@@ -12,21 +29,7 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	go func() {
-		defer wg.Done()
-		consumer, err := handler.NewPaymentConsumer()
-
-		if err != nil {
-			logger.Fatalf("Failed to create Kafka consumer: %v", err)
-		}
-
-		defer consumer.Close()
-
-		logger.Println("Starting Kafka consumer...")
-		if err := consumer.Start(); err != nil {
-			logger.Fatalf("Failed to start Kafka consumer: %v", err)
-		}
-	}()
+	go execution(&wg, logger)
 
 	wg.Wait()
 }
